7079a671fd7c158ef18cc528496d088aaca6e4b3
Sedov Dmitry <sedov.dmitry1@gmail.com>
2016-11-11 23:41:34 +0300

    completed Task 2C

--- src/index.js
+++ src/index.js
@@ -15,25 +15,46 @@ app.get('/task2A', (req, res) => {
 });
 
 app.get('/task2B', (req, res) => {
-  const fullname = req.query.fullname || '     ';
+  const fullname = req.query.fullname.trim() || '     ';
   const array = fullname.split(' ');
-  const re = /^.*[\d_/-].*$/gi;
+  const re = /^.*[\d_/-].*$/;
   if (fullname.match(re)) {
     res.send('Invalid fullname');
+  } else {
+    switch (array.length) {
+      case 3:
+        res.send(array[2] + ' ' + array[0].charAt(0) + '. ' + array[1].charAt(0) + '.');
+        break;
+      case 2:
+        res.send(array[1] + ' ' + array[0].charAt(0) + '.');
+        break;
+      case 1:
+        res.send(array[0]);
+        break;
+      default:
+        res.send('Invalid fullname');
+    }
   }
-  switch (array.length) {
-    case 3:
-      res.send(array[2] + ' ' + array[0].charAt(0) + '. ' + array[1].charAt(0) + '.');
-      break;
-    case 2:
-      res.send(array[1] + ' ' + array[0].charAt(0) + '.');
-      break;
-    case 1:
-      res.send(array[0]);
-      break;
-    default:
-      res.send('Invalid fullname');
+});
+
+app.get('/task2C', (req, res) => {
+  const username = req.query.username;
+  let result = username;
+  if (result.indexOf('/') >= 0) {
+    const pattern = /\/([\w\d?=/-]*)$/gi;
+    const match = pattern.exec(username);
+    result = match[1];
   }
+  if (result.indexOf('?') >= 0) {
+    result = result.substring(0, result.indexOf('?'));
+  }
+  if (result.indexOf('/') >= 0) {
+    result = result.substring(0, result.indexOf('/'));
+  }
+  if (result.indexOf('@') !== 0) {
+    result = '@' + result;
+  }
+  res.send(result);
 });
 
 app.listen(3000, () => {
